fa5373c429bbbb02c60d23a1beedaa236b55c318
sensible-heart <alexandra.makepeace@gmail.com>
2016-03-02 13:41:04 -0700

    Fixed a bug where non altered answers did not trigger the end sequence.

--- SensoryUnderload.py
+++ SensoryUnderload.py
@@ -1,7 +1,7 @@
 import pygame, pygame.event, string, random, os, pygame.mixer
 from pygame.locals import *
 
-answers = ["valuable", "cause", "banner", "legacy", "research", "information", "empire", "vanquished enemie", "money", "art", "music", "gratitude", "charity", "statue", "inspiration", "friendship", "kindness", "family", "love", "children", "kid", "questions", "debt", "worries", "worry", "relationships", "positivity", "negativity", "responsibility", "bitterness", "trust", "regret", "regret", "yearning", "work", "undesire", "interest", "passion", "loneliness", "lonely", "original", "originality", "demeanor", "good spelling", "people", "parent", "dread", "death", "greatness", "ghost", "gear", "building", "buildings", "heart", "home", "joke", "arcitecture", "sarcasm", "film", "video game", "movie", "writing", "poem", "rap", "voice", "sing", "singing", "reference game", "reference", "loved", "cooking", "baking", "career", "accomplishment", "nothing", "everything", "myself", "body", "melancholy", "sadness", "fame", "fortune", "humanity", "work"]
+answers = ["me", "valuable", "cause", "banner", "legacy", "research", "information", "empire", "vanquished enemie", "money", "art", "music", "gratitude", "charity", "statue", "inspiration", "friendship", "kindness", "family", "love", "children", "kid", "questions", "debt", "worries", "worry", "relationships", "positivity", "negativity", "responsibility", "bitterness", "trust", "regret", "regret", "yearning", "work", "undesire", "interest", "passion", "loneliness", "lonely", "original", "originality", "demeanor", "good spelling", "people", "parent", "dread", "death", "greatness", "ghost", "gear", "building", "buildings", "heart", "home", "joke", "arcitecture", "sarcasm", "film", "video game", "movie", "writing", "poem", "rap", "voice", "sing", "singing", "reference game", "reference", "loved", "cooking", "baking", "career", "accomplishment", "nothing", "everything", "myself", "body", "melancholy", "sadness", "fame", "fortune", "humanity", "work"]
 nouns = ["my ", "a ", "the ", "being "]
 all_possible_answers = []
 global tracksPlaying
@@ -13,12 +13,13 @@ def generate_possible_answers():
   global answers
   global all_possible_answers
   global nouns
+  #print "I AM FO SHO GIVING POSSIBLE ANSWERS"
   plural = "s"
   plural_list = [s + plural for s in answers]
   #print plural_list
   noun_combo = [noun + answer for noun in nouns for answer in answers]
   #print noun_combo
-  all_possible_answers =  noun_combo + plural_list + answers
+  all_possible_answers =  answers + noun_combo + plural_list
   #print all_possible_answers
 
 def get_key():
@@ -34,15 +35,16 @@ def play_track(trackNumber):
   global tracksPlaying
   filename = "SensoryUnderload/InBFlat" + trackNumber + ".ogg"
   abspath = os.path.abspath(filename)
-  #print "I am your file name, yo " + filename  
+  #print "I am your file name, yo " + filename
   sound = pygame.mixer.Sound(abspath)
   tracksPlaying.append(trackNumber)
   if trackNumber == "20":
+    end_music()
     pygame.mixer.music.load(filename)
     pygame.mixer.music.play(0)
   else:
     sound.play(-1)
-    currentSounds.append(sound)  
+    currentSounds.append(sound)
 
 def add_music():
   randomTrackNumber = random.randint(1,19)
@@ -50,12 +52,12 @@ def add_music():
     randomTrackNumber = random.randint(1,19)
   #print str(randomTrackNumber) + " this is the track number."
   play_track(str(randomTrackNumber))
-  print tracksPlaying
-  print currentSounds
-  
+  #print tracksPlaying
+  #print currentSounds
+
 def end_music():
   global tracksPlaying
-  global currentSounds  
+  global currentSounds
   for sound in currentSounds:
     sound.stop()
   tracksPlaying = []
@@ -63,7 +65,7 @@ def end_music():
 
 def is_answer(answer):
   matches = 0
-  print answer + " ANSWER IS HERE."
+  #print answer + " ANSWER IS HERE."
   continueString = 1
   for word in all_possible_answers:
     #print(word + " AM YOUR WORD " , answer)
@@ -77,8 +79,8 @@ def is_answer(answer):
       matches = 1
       add_music()
       break
-  if not matches:  
-    end_music() 
+  if not matches:
+    end_music()
     continueString = 0
   return continueString
 
@@ -101,24 +103,24 @@ def get_input():
   while 1:
     inkey = get_key()
     if inkey == K_RETURN:
-      return 1    
+      return 1
     if len(tracksPlaying) == 1 and tracksPlaying[0] == "20":
       if not pygame.mixer.music.get_busy():
         #print "done"
         current_string = []
-        end_music()       
+        end_music()
         current_string = handle_input(inkey,current_string)
       continue
-    else: 
+    else:
         current_string = handle_input(inkey,current_string)
-    
+
 def main():
     #print "start"
     pygame.init()
     #pygame.mixer.init()
     size = width, height = 320, 240
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('What Will You Leave Behind?')    
+    pygame.display.set_caption('What Will You Leave Behind?')
     while 1:
       done = get_input()
       if done:
